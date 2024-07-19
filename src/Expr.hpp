4753e975c6df0c1ff30de781aec5283f3f150730
Aidan <87229829+AidanJ@users.noreply.github.com>
2024-07-19 16:20:31 +0300

    feat: initial expressions representation interface

---
+++ src/Expr.hpp
@@ -0,0 +1,99 @@
+#pragma once
+#include <fmt/core.h>
+#include <memory>
+#include <variant>
+
+#include "Token.hpp"
+
+template <class... Ts> struct overloads : Ts... {
+  using Ts::operator()...;
+};
+
+template <class... Ts> overloads(Ts...) -> overloads<Ts...>;
+
+// NOTE: Recursive types have to be allocated on the heap since they can
+// represent an infinite amount of expressions
+namespace Expr {
+  struct Literal;
+  struct Grouping;
+  struct Unary;
+  struct Binary;
+
+  // Might be a good idea forcing explicit `make_shared` calls instead of hiding
+  // heap allocation inside the expression interfaces
+  using GroupingPtr = std::shared_ptr<Grouping>;
+  using UnaryPtr = std::shared_ptr<Unary>;
+  using BinaryPtr = std::shared_ptr<Binary>;
+
+  using T = std::variant<Literal, BinaryPtr, UnaryPtr, GroupingPtr>;
+
+  // TODO: Make Literal consistent with the others? or should terminal
+  // expressions not have `init` as an initialization option
+  struct Literal {
+    Token const token;
+  };
+
+  struct Grouping {
+    // `detail::Grouping` can just be referred as `Grouping` but might cause
+    // misunderstanding
+    T expression;
+    static inline auto init(T expression) -> std::shared_ptr<Grouping> {
+      // TODO: Check if copy constructed by using aggregate initialization
+      return std::make_shared<Grouping>(
+          Grouping{.expression = std::move(expression)}
+      );
+    }
+  };
+
+  struct Unary {
+    Token const operation;
+    T expression;
+
+    static inline auto
+    init(Token operation, T expression) -> std::shared_ptr<Unary> {
+      return std::make_shared<Unary>(Unary{
+          .operation = std::move(operation), .expression = std::move(expression)
+      });
+    }
+  };
+
+  struct Binary {
+    T left;
+    Token const operation;
+    T right;
+
+    static inline auto
+    init(T left, Token operation, T right) -> std::shared_ptr<Binary> {
+      return std::make_shared<Binary>(Binary{
+          .left = std::move(left),
+          .operation = std::move(operation),
+          .right = std::move(right)
+      });
+    }
+  };
+
+  inline auto display(T const& expression) -> std::string {
+    return std::visit(
+        overloads{
+            [](Literal const& expr) { return expr.token.display(); },
+            [](GroupingPtr const& expr) -> std::string {
+              return fmt::format("({})", display(expr->expression));
+            },
+            [](UnaryPtr const& expr) -> std::string {
+              return fmt::format(
+                  "({} {})", expr->operation.display(),
+                  display(expr->expression)
+              );
+            },
+            [](BinaryPtr const& expr) -> std::string {
+              return fmt::format(
+                  "({} {} {})", expr->operation.display(), display(expr->left),
+                  display(expr->right)
+              );
+            }
+        },
+        expression
+    );
+  };
+
+} // namespace Expr
